b9e21554d5e35a26514bf213117a168e96a732e6
shaonianzhentan <ilovevar@gmail.com>
2021-12-12 00:32:02 +0800

    完成插件功能

--- custom_components/panel_iframe/__init__.py
+++ custom_components/panel_iframe/__init__.py
@@ -10,18 +10,25 @@ CONFIG_SCHEMA = cv.deprecated(DOMAIN)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.http.register_static_path("/panel_iframe_www", hass.config.path("custom_components/" + DOMAIN + "/www"), False)
     # 添加面板
-    cfg = entry.data
+    cfg = entry.options
     url_path = entry.entry_id
+    title = entry.title
     mode = cfg.get('mode')
-    title = cfg.get('title')
     icon = cfg.get('icon')
     url = cfg.get('url')
     require_admin = cfg.get('require_admin')
-    hass.components.frontend.async_register_built_in_panel("iframe", title, icon, url_path,
-        {"url": f"/panel_iframe_www/index.html?v={VERSION}&mode={mode}&url={urllib.parse.quote(url)}"},
-        require_admin=require_admin,
-    )
+    if url is not None:
+        hass.components.frontend.async_register_built_in_panel("iframe", title, icon, url_path,
+            {"url": f"/panel_iframe_www/index.html?v={VERSION}&mode={mode}&url={urllib.parse.quote(url)}"},
+            require_admin=require_admin,
+        )
+    entry.async_on_unload(entry.add_update_listener(update_listener))
     return True
+
+async def update_listener(hass, entry):
+    """Handle options update."""
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     url_path = entry.entry_id
--- custom_components/panel_iframe/config_flow.py
+++ custom_components/panel_iframe/config_flow.py
@@ -3,10 +3,20 @@ from __future__ import annotations
 from typing import Any
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow
 from homeassistant.data_entry_flow import FlowResult
 
+import homeassistant.helpers.config_validation as cv
+from homeassistant.core import callback
+from homeassistant.config_entries import ConfigFlow, OptionsFlow, ConfigEntry
+
 from .const import DOMAIN
+
+mode_list = {
+    '0': '默认',
+    '1': '全屏',
+    '2': '新页面',
+    '3': '内置页面'
+}
 
 class SimpleConfigFlow(ConfigFlow, domain=DOMAIN):
 
@@ -18,22 +28,39 @@ class SimpleConfigFlow(ConfigFlow, domain=DOMAIN):
 
         if user_input is None:
             errors = {}
-            mode_list = {
-                '0': '默认',
-                '1': '全屏',
-                '2': '新页面',
-                '3': '内置页面'
-            }
             DATA_SCHEMA = vol.Schema({
                 vol.Required("title"): str,
-                vol.Required("icon", default='mdi:link-box-outline'): str,
-                vol.Required("url"): str,
-                vol.Required("require_admin", default=False): bool,
-                vol.Required("mode", default=['0']): vol.In(mode_list),
             })
             return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
 
-        user_input['title'] = user_input['title'].strip()
+        return self.async_create_entry(title=user_input['title'], data=user_input)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(entry: ConfigEntry):
+        return OptionsFlowHandler(entry)
+
+
+class OptionsFlowHandler(OptionsFlow):
+    def __init__(self, config_entry: ConfigEntry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        return await self.async_step_user(user_input)
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+        if user_input is None:
+            options = self.config_entry.options
+            errors = {}
+            DATA_SCHEMA = vol.Schema({
+                vol.Required("icon", default=options.get('icon', 'mdi:link-box-outline')): str,
+                vol.Required("url", default=options.get('url', '')): str,
+                vol.Required("require_admin", default=options.get('require_admin', False)): bool,
+                vol.Required("mode", default=options.get('mode', '0')): vol.In(mode_list),
+            })
+            return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
+        # 选项更新
         user_input['icon'] = user_input['icon'].strip().replace('mdi-', 'mdi:')
         user_input['url'] = user_input['url'].strip()
-        return self.async_create_entry(title=user_input['title'], data=user_input)
+        return self.async_create_entry(title='', data=user_input)
